f98c39617c1bc9a65acff4e0708f03d4967c6d7c
scapps00 <hello@samanthacapps.com>
2018-05-23 18:30:54 -0400

    moving numbers around works

--- numbers/platforms/ios/Numbers.xcarchive/Products/Applications/Numbers.app/www/js/format.js
+++ numbers/platforms/ios/Numbers.xcarchive/Products/Applications/Numbers.app/www/js/format.js
@@ -1,15 +1,7 @@
 var widthTable = $(".grid").width();
 $(".grid").css("height", widthTable + "px");
-$(".grid").css("max-height", widthTable + "px");
-$(".grid").css("min-height", widthTable + "px");
 
-$(".choices").width($(".grid").width());
-$(".choices").css("max-width", $(".grid").width() + "px");
-$(".choices").css("min-width", $(".grid").width() + "px");
-
-$(".choices").height($(".choices").width() / 6 * 4);
-$(".choices").css("max-height", $(".choices").width() / 6 * 4 + "px");
-$(".choices").css("min-height", $(".choices").width() / 6 * 4 + "px");
+$(".choices").height($(".choices").width() / 5 * 4);
 
 // $(".choices tr td").width($(".choices").width() / 6);
 // $(".choices tr td").css("max-width", $(".choices").width() / 6 + "px");
--- numbers/www/js/moveNum.js
+++ numbers/www/js/moveNum.js
@@ -1,11 +1,18 @@
-$(".choices tr td").click(function(event1) {
-    event1.preventDefault();
-    $(".choices tr td:not(.blank)").css("background-color", "#ebebeb");
-    $(event1.target).css("background", "#ecbc76");
-    $(".grid tr td").click(function(event2) {
-        event2.preventDefault();
-        $(event2.target).text($(event1.target).text());
-        $(event1.target).text("");
-        $(event1.target).css("background", "#ebebeb");
-    })
-});
+function assignClick() {
+    $("td:not(:empty)").click(function(event1) {
+        event1.preventDefault();
+        $("td:not(.blank)").css("background-color", "#ebebeb");
+        $(event1.target).css("background", "#ecbc76");
+        $("td:empty:not(.blank)").click(function(event2) {
+            event2.preventDefault();
+            $(event2.target).text($(event1.target).text());
+            $(event1.target).text("");
+            $(event1.target).css("background", "#ebebeb");
+            $("td").unbind();
+            assignClick();
+        });
+    });
+}
+
+assignClick();
+
