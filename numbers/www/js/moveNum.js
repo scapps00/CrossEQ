b82a21a73d2cf4d93062df42539a099b4e5c8892
scapps00 <hello@samanthacapps.com>
2018-05-23 18:37:52 -0400

    moving numbers works

--- numbers/www/js/moveNum.js
+++ numbers/www/js/moveNum.js
@@ -1,6 +1,7 @@
 function assignClick() {
     $("td:not(:empty)").click(function(event1) {
         event1.preventDefault();
+        $("td[background-color=#ebebeb]").unbind();
         $("td:not(.blank)").css("background-color", "#ebebeb");
         $(event1.target).css("background", "#ecbc76");
         $("td:empty:not(.blank)").click(function(event2) {
