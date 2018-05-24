59cbdab9ce39f6755035dbcbbd587157b0179717
scapps00 <hello@samanthacapps.com>
2018-05-23 21:58:02 -0400

    equals working

--- numbers/platforms/ios/www/js/moveNum.js
+++ numbers/platforms/ios/www/js/moveNum.js
@@ -1,23 +1,5 @@
-// function clear(selected) {
-//     selected.unbind();
-//     selected.removeClass("selected");
-//     selected.click(function(event1) {
-//         event1.preventDefault();
-//         $("td:not(.blank)").css("background-color", "#ecbc76");
-//         $(event1.target).css("background-color", "#ecbc76");
-//         $("td:empty:not(.blank)").click(function(event2) {
-//             event2.preventDefault();
-//             $(event2.target).text($(event1.target).text());
-//             $(event1.target).text("");
-//             $(event1.target).css("background", "#ebebeb");
-//             $("td").unbind();
-//             assignClick();
-//         });
-//     });
-// }
-
 function assignClick() {
-    $("td:not(:empty)").click(function(event1) {
+    $("td:not(:empty):not(.equal)").click(function(event1) {
         event1.preventDefault();
         $(".selected").css("background-color", "#ebebeb");
         $("td:empty:not(.blank)").unbind();
