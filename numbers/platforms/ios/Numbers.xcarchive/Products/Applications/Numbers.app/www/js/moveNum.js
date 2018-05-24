b253ba7dccbfd24a0415d061ea668e519c2703f2
scapps00 <hello@samanthacapps.com>
2018-05-23 20:59:05 -0400

    moving numbers working

--- numbers/platforms/ios/Numbers.xcarchive/Products/Applications/Numbers.app/www/js/moveNum.js
+++ numbers/platforms/ios/Numbers.xcarchive/Products/Applications/Numbers.app/www/js/moveNum.js
@@ -1,13 +1,35 @@
+// function clear(selected) {
+//     selected.unbind();
+//     selected.removeClass("selected");
+//     selected.click(function(event1) {
+//         event1.preventDefault();
+//         $("td:not(.blank)").css("background-color", "#ecbc76");
+//         $(event1.target).css("background-color", "#ecbc76");
+//         $("td:empty:not(.blank)").click(function(event2) {
+//             event2.preventDefault();
+//             $(event2.target).text($(event1.target).text());
+//             $(event1.target).text("");
+//             $(event1.target).css("background", "#ebebeb");
+//             $("td").unbind();
+//             assignClick();
+//         });
+//     });
+// }
+
 function assignClick() {
     $("td:not(:empty)").click(function(event1) {
         event1.preventDefault();
-        $("td:not(.blank)").css("background-color", "#ebebeb");
-        $(event1.target).css("background", "#ecbc76");
+        $(".selected").css("background-color", "#ebebeb");
+        $("td:empty:not(.blank)").unbind();
+        $(".selected").removeClass("selected");
+        $(event1.target).addClass("selected");
+        $(".selected").css("background-color", "#ecbc76");
         $("td:empty:not(.blank)").click(function(event2) {
             event2.preventDefault();
-            $(event2.target).text($(event1.target).text());
-            $(event1.target).text("");
-            $(event1.target).css("background", "#ebebeb");
+            $(event2.target).text($(".selected").text());
+            $(".selected").text("");
+            $(".selected").css("background-color", "#ebebeb");
+            $(".selected").removeClass(".selected");
             $("td").unbind();
             assignClick();
         });
