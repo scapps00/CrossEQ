765883f5321c4a865808ecae2027045538c7e80e
scapps00 <hello@samanthacapps.com>
2018-05-24 02:23:36 -0400

    some generation working

--- crosseq/www/js/solutionGen.js
+++ crosseq/www/js/solutionGen.js
@@ -9,25 +9,26 @@ function randomOp() {
 }
 
 function findMultTotal(divisor) {
-    var total = randomNum((divisor * 2), 40);
+    var total = randomNum(divisor, 80);
     if (total % divisor === 0) {
         return total;
     } else {
-        findMultTotal(divisor);
+        return findMultTotal(divisor);
     }
 }
 
 function findDivisor(total) {
-    var divisor = randomNum(1, (total / 2));
+    var divisor = randomNum(1, total);
     if (total % divisor === 0) {
         return divisor;
     } else {
-        findDivisor(total);
+        return findDivisor(total);
     }
 }
 
 var solution = [];
 solution[23] = randomOp();
+solution[32] = randomOp();
 
 function solution2() {
     if (solution[23] === "-") {
@@ -61,9 +62,71 @@ function solution2() {
     }
 }
 
+function solution3() {
+    if (solution[32] === "-") {
+        solution[31] = randomNum(2, 40);
+        solution[33] = randomNum(1, solution[31]);
+        solution[35] = solution[31] - solution[33];
+    } else if (solution[32] === "+") {
+        solution[31] = randomNum(1, 20);
+        if (solution[31] === 20) {
+            solution[33] = randomNum(1,19);
+            solution[35] = solution[31] + solution[33];
+        } else {
+            solution[33] = randomNum(1,20);
+            solution[35] = solution[31] + solution[33];
+        }
+    } else if (solution[32] === "×") {
+        var picker = randomNum(1,2);
+        if (picker === 1) {
+            solution[31] = randomNum(1, 19);
+            solution[35] = findMultTotal(solution[31]);
+            solution[33] = solution[35] / solution[31];
+        } else {
+            solution[33] = randomNum(1, 19);
+            solution[35] = findMultTotal(solution[33]);
+            solution[31] = solution[35] / solution[33];
+        }
+    } else if (solution[32] === "÷") {
+        solution[31] = randomNum(2, 40);
+        solution[33] = findDivisor(solution[31]);
+        solution[35] = solution[31] / solution[33];
+    }
+}
+
+function solution13() {
+    if (solution[23] === "-") {
+        solution[13] = randomNum(solution[33], 41);
+        solution[53] = solution[13] - solution[33];
+    } else if (solution[23] === "+") {
+        solution[13] = randomNum(1, 20);
+        solution[53] = solution[13] + solution[33];
+    } else if (solution[23] === "×") {
+        solution[53] = findMultTotal(solution[33]);
+        solution[13] = solution[53] / solution[33];
+    } else if (solution[23] === "÷") {
+        solution[13] = findMultTotal(solution[33]);
+        solution[53] = solution[13] / solution[33];
+    }
+}
+
+function solution42() {
+
+}
+
 solution2();
+solution3();
+solution13();
 
 $("[class='2-2']").text(solution[22]);
 $("[class='2-3']").text(solution[23]);
 $("[class='2-4']").text(solution[24]);
 $("[class='2-6']").text(solution[26]);
+
+$("[class='3-1']").text(solution[31]);
+$("[class='3-2']").text(solution[32]);
+$("[class='3-3']").text(solution[33]);
+$("[class='3-5']").text(solution[35]);
+
+$("[class='1-3']").text(solution[13]);
+$("[class='5-3']").text(solution[53]);
