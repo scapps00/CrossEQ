3406da77cb82d7ddaea9f81713f3498159100e4a
scapps00 <hello@samanthacapps.com>
2018-06-10 12:14:54 -0400

    aspect ratio on ipad working

--- crosseq/platforms/ios/www/js/format.js
+++ crosseq/platforms/ios/www/js/format.js
@@ -1,3 +1,8 @@
+var gameHeight = $(window).height();
+$(".entireContainer").height(gameHeight);
+var gameWidth = $(window).height() / 1.719;
+$(".entireContainer").width(gameWidth);
+
 var widthTable = $(".grid").width();
 $(".grid").css("height", widthTable + "px");
 
